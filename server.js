eafc36070bf92c0807b65a377746fd07da2ada9f
s2 <s@ireoo.com>
2014-01-23 20:05:25 +0800

    更新程序

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ server = http.createServer(function(req, res){
     o.log(path);   //显示用户打开扫描页面
     fs.readFile(__dirname + path, function(err, data){
         if (err) return send404(res);
-        res.writeHead(200, {'Content-Type': path.substr(path.length-2) == 'js' ? 'text/javascript, charset=UTF-8' : 'text/html, charset=UTF-8'});
+        res.writeHead(200, {'Content-Type': path.substr(path.length - 2) == 'js' ? 'text/javascript, charset=UTF-8' : 'text/html, charset=UTF-8'});
         res.write(data, 'utf8');
         res.end();
     });
